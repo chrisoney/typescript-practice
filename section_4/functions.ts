aa26f322b6821d98fe1e2e71f88af47dc59d234f
chrisoney <christopher.oney@yahoo.com>
2021-11-16 15:54:45 -0800

    anonymous functions syntax

--- section_4/functions.ts
+++ section_4/functions.ts
@@ -1,3 +1,12 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+
+function divide(a: number,b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+}
