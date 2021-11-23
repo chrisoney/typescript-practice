76f0846f8e87ba538942606224e1dec711a12ee2
chrisoney <christopher.oney@yahoo.com>
2021-11-22 18:46:25 -0800

    finish classes content

--- section_8/classes.ts
+++ section_8/classes.ts
@@ -1,8 +1,9 @@
 class Vehicle {
-  color: string;
-  constructor(color:string) {
-    this.color = color;
-  }
+  // color: string;
+  // constructor(color:string) {
+  //   this.color = color;
+  // }
+  constructor(public color: string) {}
 
   honk(): void {
     console.log('beep');
@@ -11,6 +12,9 @@ class Vehicle {
 const vehicle = new Vehicle('orange');
 
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   private drive(): void {
     console.log('vroom')
   }
