82ea5a8cb62a0f755114c171091355f43975064b
chrisoney <christopher.oney@yahoo.com>
2021-11-23 22:19:08 -0800

    increase reusability and protect map info

--- section_9/maps/src/CustomMap.ts
+++ section_9/maps/src/CustomMap.ts
@@ -1,7 +1,7 @@
 export class CustomMap {
-  googleMap: google.maps.Map;
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+  private googleMap: google.maps.Map;
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: {
         lat: 0,
