c394c830c75968a06348c00d8926a90b1ffe1fbe
chrisoney <christopher.oney@yahoo.com>
2021-11-26 20:34:09 -0800

    get ready to add NumbersCollection class

---
+++ section_10/sort/src/Sorter.ts
@@ -0,0 +1,39 @@
+export class Sorter {
+  constructor(public collection: /* fix me later */) { }
+  
+  // Bubble sort? Look up later
+  sort(): void {
+    const { length } = this.collection;
+    
+    for (let i = 0; i < length; i++){
+      for (let j = 0; j < length - i - 1; j++){
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+
+/*
+// Random Notes
+
+// If collection is an array of numbers
+// Use this syntax for anything that is not a number, string, or boolean, by referencing the constructor function
+if (this.collection instanceof Array) {
+  if (this.collection[j] > this.collection[j + 1]) {
+    const temp = this.collection[j];
+    this.collection[j] = this.collection[j + 1];
+    this.collection[j + 1] = temp;
+  }
+}
+// Only use this syntax for number, string, and boolean
+if (typeof this.collection === 'string') {
+  
+}
+
+*/
