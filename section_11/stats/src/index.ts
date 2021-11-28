302503ab0f7e6d52e2d584454f77eedd3c069012
chrisoney <christopher.oney@yahoo.com>
2021-11-28 07:17:15 -0800

    faulty approach

--- section_11/stats/src/index.ts
+++ section_11/stats/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hey')
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+}).split('\n').map((ele: string): string[] => ele.split(','));
+
+let manUnitedWins = 0;
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+for (let match of matches) {
+  if ((match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)) {
+    manUnitedWins++
+      }
+}
+
+console.log(manUnitedWins)
