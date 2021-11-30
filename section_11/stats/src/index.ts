c163186ab3477b5d73a3d9a11c7dbb545675a6fb
chrisoney <christopher.oney@yahoo.com>
2021-11-29 20:05:34 -0800

    start summary class

--- section_11/stats/src/index.ts
+++ section_11/stats/src/index.ts
@@ -8,6 +8,7 @@ const csvFileReader = new CsvFileReader('football.csv');
 // Create an instance of MatchReader and pass in something satisfying the Datareader interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
 let manUnitedWins = 0;
 
 for (let match of matchReader.matches) {
