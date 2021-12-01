9d6e3b4bad31fedda196a283366c223e597064e4
chrisoney <christopher.oney@yahoo.com>
2021-12-01 13:56:45 -0800

    fix interfact for set method

--- section_13/web/src/index.ts
+++ section_13/web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({name: 'Chris', age: 30});
 
-console.log(user.get('age'))
-console.log(user.get('name'))
+user.set({ name: 'chris' });
+
+console.log(user.get('age'));
+console.log(user.get('name'));
