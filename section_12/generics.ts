65c89fc87a5f03f8da720b271816c2a0134ec67d
chrisoney <christopher.oney@yahoo.com>
2021-12-01 12:48:36 -0800

    finish section 12

--- section_12/generics.ts
+++ section_12/generics.ts
@@ -40,3 +40,27 @@ function printAnything<T>(arr: T[]):void {
     console.log(arr[i])
   }
 }
+
+// Generic constraints (it's all about that T extends Printable)
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for(let i = 0; i < arr.length;i++){
+    arr[i].print();
+  }
+}
