7e91521bf866e791cf6ccec92e8f19032d9a1d8c
chrisoney <christopher.oney@yahoo.com>
2021-12-01 13:54:53 -0800

    add set method

--- section_13/web/dist/src.f10117fe.js
+++ section_13/web/dist/src.f10117fe.js
@@ -117,9 +117,45 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-console.log('sup');
-},{}],"../../../../.nvm/versions/node/v12.22.7/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.User = void 0;
+
+var User =
+/** @class */
+function () {
+  function User(data) {
+    this.data = data;
+  }
+
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+
+  return User;
+}();
+
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./models/User");
+
+var user = new User_1.User({
+  name: 'Chris',
+  age: 30
+});
+console.log(user.get('age'));
+console.log(user.get('name'));
+},{"./models/User":"src/models/User.ts"}],"../../../../.nvm/versions/node/v12.22.7/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +183,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56715" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57015" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- section_13/web/src/index.ts
+++ section_13/web/src/index.ts
@@ -1,1 +1,6 @@
-console.log('sup')
+import { User } from "./models/User";
+
+const user = new User({name: 'Chris', age: 30});
+
+console.log(user.get('age'))
+console.log(user.get('name'))
