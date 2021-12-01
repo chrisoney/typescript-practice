58d2368d4f4ce81b79e70c100f0e153b339fa349
chrisoney <christopher.oney@yahoo.com>
2021-12-01 12:37:24 -0800

    generics with functions

--- section_12/generics.ts
+++ section_12/generics.ts
@@ -4,3 +4,39 @@ class ArrayOfNumbers {
     return this.collection[index];
   }
 }
+
+class ArrayOfStrings{
+  constructor(public collection: string[]) {}
+
+  get(index:number): string {
+    return this.collection[index];
+  } 
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) { }
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+// Example of generics with functions
+
+function printStrings(arr: string[]): void{
+  for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+  }
+}
+
+function printNumbers(arr: number[]): void{
+  for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+  }
+}
+
+function printAnything<T>(arr: T[]):void {
+  for(let i = 0; i < arr.length;i++){
+    console.log(arr[i])
+  }
+}
