8072be32b14761f4e2ee4cdfe0729d5a3e80e3fc
chrisoney <christopher.oney@yahoo.com>
2021-12-02 10:11:57 -0800

    reintegrate event logic and test

--- section_13/web/src/index.ts
+++ section_13/web/src/index.ts
@@ -3,3 +3,8 @@ import { User } from "./models/User";
 const user = new User({ name: "New Record", age: 0 });
 
 user.save();
+user.events.on('change', () => {
+  console.log('test')
+});
+
+user.events.trigger('change');
