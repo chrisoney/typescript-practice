cc2813743dfe3bec2a6cdae76407f2907bad1738
chrisoney <christopher.oney@yahoo.com>
2022-01-09 19:05:06 -0800

    finish main part of code

--- section_13/web/src/index.ts
+++ section_13/web/src/index.ts
@@ -2,12 +2,18 @@ import axios, { AxiosResponse } from 'axios';
 import { Collection } from './models/Collection';
 import { User, UserProps } from './models/User';
 import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
+
+
 
 const user = User.buildUser({ name: 'Name', age: 20 })
 const root = document.getElementById('root');
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  // const userForm = new UserForm(root, user);
+  // userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit)
 } else {
   throw new Error('Root element not found.')
 }
