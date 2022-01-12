dc4e7731eb6a5f2a5413fdee7a3d18350884c5a6
chrisoney <christopher.oney@yahoo.com>
2022-01-12 14:41:32 -0800

    start setting up router

--- section_14/server/src/index.ts
+++ section_14/server/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.use(router);
+
+
+const port = 3000;
+app.listen(port, () => { console.log(`Listening on port ${port}`)})
