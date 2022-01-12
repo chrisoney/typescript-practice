1e37267763d8636e7fc64f9a54d3b2f60c551025
chrisoney <christopher.oney@yahoo.com>
2022-01-12 15:16:47 -0800

    add logout route

--- section_14/server/src/routes/loginRoutes.ts
+++ section_14/server/src/routes/loginRoutes.ts
@@ -31,9 +31,32 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
     req.session = { loggedIn: true };
     res.redirect('/')
   } else {
-    res.send('Invalid email or password.')
+    res.send('Invalid email or password.');
   }
 
 })
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href='/logout'>Logout</a>
+      </div>
+    `)
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href='/login'>Log In</a>
+      </div>
+    `)
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
 
 export { router };
