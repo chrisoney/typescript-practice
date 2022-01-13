24d4eac534d83d861e42ddacc67bb15cb2520073
chrisoney <christopher.oney@yahoo.com>
2022-01-13 09:45:54 -0800

    add first decorator

--- section_15/features_new/decorators.ts
+++ section_15/features_new/decorators.ts
@@ -1,11 +1,16 @@
 class Boat {
-  color: string;
+  color: string = '';
 
   get formattedColor(): string {
     return `This boat's color is ${this.color}`
   }
-
+  @testDecorator
   pilot(): void {
     console.log('Swish');
   }
+}
+
+function testDecorator(target: any, key: string): void {
+  console.log('Target: ', target);
+  console.log('Key: ', key);
 }
