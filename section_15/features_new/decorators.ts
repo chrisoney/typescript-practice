26d5d298fc0043fbbd646fe16afc175fcbf9d92d
chrisoney <christopher.oney@yahoo.com>
2022-01-13 12:40:54 -0800

    catch up old files

--- section_15/features_new/decorators.ts
+++ section_15/features_new/decorators.ts
@@ -1,16 +1,27 @@
 class Boat {
-  color: string = '';
+  color: string = 'red';
 
   get formattedColor(): string {
     return `This boat's color is ${this.color}`
   }
-  @testDecorator
+  @logError
   pilot(): void {
+    throw new Error('BLAH!')
     console.log('Swish');
   }
 }
-
-function testDecorator(target: any, key: string): void {
-  console.log('Target: ', target);
-  console.log('Key: ', key);
+// Target will be the prototype of Boat (in this case), key will be the name of whatever we use for the second argument. in this case pilot (could also be formattedColor or color)
+// Third argument would be property descriptor (object)
+// Important note: Decorators are applied when code for the class is ran (NOT when an instance is created)
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+  desc.value = function() {
+    try {
+      method();
+    } catch(e) {
+      console.log('Oops, boat sunk.')
+    }
+  }
 }
+
+new Boat().pilot()
