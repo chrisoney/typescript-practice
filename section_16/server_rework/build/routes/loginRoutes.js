289da6a3a20d5a6bf2dd22531075825284b1507b
chrisoney <christopher.oney@yahoo.com>
2022-01-15 17:20:10 -0800

    copying over express project for rework

---
+++ section_16/server_rework/build/routes/loginRoutes.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    else {
+        res.status(403).send('Not permitted');
+    }
+}
+const router = (0, express_1.Router)();
+exports.router = router;
+router.get('/login', (req, res) => {
+    res.send(`
+    <form method='POST'>
+      <div>
+        <label>Email</label>
+        <input name='email' />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name='password' type='password' />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    // first the typeguard, by checking the existence of the two attributes
+    if (email && password && email === 'chris@me.com' && password === 'hunter2') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password.');
+    }
+});
+router.get('/', (req, res) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href='/logout'>Logout</a>
+      </div>
+    `);
+    }
+    else {
+        res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href='/login'>Log In</a>
+      </div>
+    `);
+    }
+});
+router.get('/logout', (req, res) => {
+    req.session = undefined;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, (req, res) => {
+    res.send('Welcome to the protected route, logged in user.');
+});
