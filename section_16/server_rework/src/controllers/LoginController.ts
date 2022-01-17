faa83172709a200682fd5ef4e4670f32c1fac7ce
chrisoney <christopher.oney@yahoo.com>
2022-01-17 15:47:42 -0800

    catch up code while trying to find fix

--- section_16/server_rework/src/controllers/LoginController.ts
+++ section_16/server_rework/src/controllers/LoginController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 
 import { get } from './decorators/routes';
+import { controller } from './decorators/controller';
 
-// @controller('/')
+@controller('/auth')
 class LoginController {
   @get('/login')
   getLogin(req: Request, res: Response): void {
--- section_16/server_rework/src/index.ts
+++ section_16/server_rework/src/index.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import { router as controllerRouter } from './controllers/decorators/controller';
+
+import './controllers/LoginController';
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['doesnotmatter'] }))
 app.use(router);
+app.use(controllerRouter);
 
 const port = 3000;
 app.listen(port, () => { console.log(`Listening on port ${port}`)})
