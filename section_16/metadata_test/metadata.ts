ef7a60e2ca72fec7056e5081a0a6675548eab462
chrisoney <christopher.oney@yahoo.com>
2022-01-16 17:24:16 -0800

    finish notes

--- section_16/metadata_test/metadata.ts
+++ section_16/metadata_test/metadata.ts
@@ -12,28 +12,26 @@ import 'reflect-metadata';
 
 // Practical below
 
-@printMetadata
+@controller
 class Plane {
   color: string = 'red';
 
-  @markFunction('supersecret')
+  @get('/login')
   fly(): void {
     console.log('vrrrrrr');
   }
 };
-function markFunction(secretInfo: string) {
+function get(path: string) {
   return function (target: any, key: string) {
-    Reflect.defineMetadata('secret', secretInfo, target, key);
+    Reflect.defineMetadata('path', path, target, key);
   }
 }
 
-function printMetadata(target: typeof Plane) {
+function controller(target: typeof Plane) {
   console.log(target)
   for (let key in target.prototype) {
-    const secret = Reflect.getMetadata('secret', target.prototype, key);
-    console.log(secret)
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    console.log(path)
+    //router.get(path, target.prototype[key])
   }
 }
-
-// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
-// console.log(secret);
