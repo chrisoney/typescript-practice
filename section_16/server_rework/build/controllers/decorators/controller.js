664e1bc84fba66880e0ae9bb9c228eb50142182b
chrisoney <christopher.oney@yahoo.com>
2022-01-17 17:30:31 -0800

    tcode out middleware decorator logic

--- section_16/server_rework/build/controllers/decorators/controller.js
+++ section_16/server_rework/build/controllers/decorators/controller.js
@@ -2,15 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = void 0;
 require("reflect-metadata");
+var MetadataKeys_1 = require("./MetadataKeys");
 var AppRouter_1 = require("../../AppRouter");
 function controller(routePrefix) {
     return function (target) {
         var router = AppRouter_1.AppRouter.getInstance();
         for (var key in target.prototype) {
             var routeHandler = target.prototype[key];
-            var path = Reflect.getMetadata('path', target.prototype, key);
+            var path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.path, target.prototype, key);
+            var method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
+            var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target.prototype, key) || [];
             if (path) {
-                router.get("".concat(routePrefix).concat(path), routeHandler);
+                router[method]("".concat(routePrefix).concat(path), middlewares, routeHandler);
             }
         }
     };
