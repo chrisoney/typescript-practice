75b537861d2689e0097feabd0db10d6df48e27e8
chrisoney <christopher.oney@yahoo.com>
2022-01-17 18:02:21 -0800

    finish bodyvalidator middlewareg

--- section_16/server_rework/src/controllers/decorators/controller.ts
+++ section_16/server_rework/src/controllers/decorators/controller.ts
@@ -2,6 +2,23 @@ import 'reflect-metadata';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
 import { AppRouter } from '../../AppRouter';
+import { NextFunction, Request, Response, RequestHandler } from 'express';
+
+function bodyValidators(keys: string[]): RequestHandler {
+  return function(req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid request.');
+      return;
+    }
+    
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send('Invalid request.');
+        return;
+      }
+    }
+  }
+}
 
 export function controller(routePrefix: string) {
   return function(target: Function) {
@@ -10,13 +27,19 @@ export function controller(routePrefix: string) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
-      const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      );
 
       const middlewares = Reflect.getMetadata(
         MetadataKeys.middleware,
         target.prototype,
         key
       ) || [];
+
+      const keys = Reflect.getMetadata();
       
       if (path) {
         router[method](`${routePrefix}${path}`, middlewares, routeHandler);
