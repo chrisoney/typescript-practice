36e1dc5af83acd6a2c1c48438ade99f8893f2baa
chrisoney <christopher.oney@yahoo.com>
2022-01-17 17:02:24 -0800

    dry up route handler code

--- section_16/server_rework/src/controllers/decorators/routes.ts
+++ section_16/server_rework/src/controllers/decorators/routes.ts
@@ -1,7 +1,16 @@
 import 'reflect-metadata';
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
+function routeBinder(method: string) {
+  return function(path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    }
   }
 }
+
+export const get = routeBinder('GET');
+export const post = routeBinder('POST');
+export const put = routeBinder('PUT');
+export const patch = routeBinder('PATCH');
+export const del = routeBinder('DELETE');
