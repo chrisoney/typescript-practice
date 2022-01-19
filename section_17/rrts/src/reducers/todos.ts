b06af3a250e158aaf80b8a2a5b5ddace799cca2d
chrisoney <christopher.oney@yahoo.com>
2022-01-18 16:28:34 -0800

    catch up reducer

--- section_17/rrts/src/reducers/todos.ts
+++ section_17/rrts/src/reducers/todos.ts
@@ -1,12 +1,18 @@
-import { Todo, FetchTodosAction, ActionTypes } from '../actions';
+import { 
+  Todo,
+  ActionTypes,
+  Action
+} from '../actions';
 
 export const todosReducer = (
   state: Todo[] = [],
-  action: FetchTodosAction
+  action: Action
 ) => {
   switch(action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+
     default:
       return state;
   }
